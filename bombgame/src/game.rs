796e6bdbd74fb949ffb92ea5cfa474e136f26c06
Captain Baguette <62989385+CaptainBaguettes@users.noreply.github.com>
2024-07-23 21:28:18 +0200

    Add files via upload
    [network] Essaie de mise en place du réseau

---
+++ bombgame/src/game.rs
@@ -0,0 +1,100 @@
+use crossterm::event::{self, Event, KeyCode};
+use crossterm::{cursor, ExecutableCommand};
+use rand::{thread_rng, Rng};
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+use std::thread::sleep;
+use std::time::Duration;
+
+use crate::map::Map;
+use crate::player::Player;
+use crate::utils::{self, GameState, Position};
+
+
+use bincode;
+
+
+pub fn run_game(bind_socket: String) -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let mut map = Map::load("assets/maps/beach_map.txt")?;
+    map.spawn_bombs(50);
+    let random_position = map.random_position();
+    let mut player = Player::new(random_position.0, random_position.1);
+    let mut key_pressed = false; // Initialisation de la variable pour suivre l'état de la touche
+    let sleep_duration = Duration::from_millis(10);
+
+    let mut stream = TcpStream::connect(bind_socket)?;
+
+    let id = thread_rng().gen_range(0..std::u64::MAX);
+
+    let mut state = GameState::new();
+
+    loop {
+        stdout.execute(cursor::Hide)?;
+        map.draw(&mut stdout)?;
+        player.draw(&mut stdout)?;
+        state.draw(&mut stdout)?;
+        stdout.flush()?;
+
+        if event::poll(Duration::ZERO)? {
+            if let Event::Key(event) = event::read()? {
+                if !key_pressed {
+                    match event.code {
+                        KeyCode::Char('q') => break,
+                        _ => {
+                            let (potential_new_x, potential_new_y) =
+                                player.calculate_new_position(event.code);
+                        
+                            if !map.is_wall(potential_new_x.into(), potential_new_y.into()) {
+                                player.move_player(event.code, &map);
+                                key_pressed = true; // Marquer la touche comme pressée
+                            }
+
+                            let new_x = player.get_x();
+                            let new_y = player.get_y();
+                            if map.is_bomb(new_x.into(), new_y.into()) {
+                                let random_position = map.random_position();
+                                player = Player::new(random_position.0, random_position.1);
+                            } else if map.is_door(new_x.into(), new_y.into()) {
+                                break;
+                            }
+                            
+                            let pos = Position::new(player.get_x(), player.get_y());
+                            let pos_update = utils::PositionMessage::new(id, pos);
+                            let encoded = bincode::serialize(&pos_update).unwrap();
+
+                            match stream.write(&encoded) {
+                                Ok(_) => (),
+                                Err(_) => println!("Error sending data")
+                            }
+                            
+
+                            let mut buffer: Vec<u8> = vec![];
+
+                            
+
+                            match stream.read(&mut buffer) {
+                                Ok(_) => (),
+                                Err(_) => println!("Error reading data")
+                            }
+
+                            if let Ok(new_state) = bincode::deserialize(&buffer) {
+                                state = new_state;
+                            };
+
+
+                        }
+                    }
+                }
+            }
+        } else {
+            key_pressed = false; // Aucun événement, marquer la touche comme non pressée
+        }
+
+        stdout.execute(cursor::Show)?;
+        sleep(sleep_duration);
+        stdout.flush()?;
+    }
+
+    Ok(())
+}
---
+++ bombgame/src/map.rs
@@ -0,0 +1,87 @@
+use crossterm::cursor;
+use rand::Rng;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Map {
+    pub layout: Vec<String>,
+}
+
+impl Map {
+    pub fn height(&self) -> usize {
+        self.layout.len()
+    }
+
+    pub fn width(&self) -> usize {
+        self.layout[0].len()
+    }
+
+    pub fn load(file_path: &str) -> io::Result<Self> {
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+        let layout = reader.lines().collect::<Result<Vec<_>, _>>()?;
+        Ok(Self { layout })
+    }
+
+    pub fn draw<W: Write>(&self, stdout: &mut W) -> io::Result<()> {
+        for (y, line) in self.layout.iter().enumerate() {
+            write!(stdout, "{}{}", cursor::MoveTo(0, y as u16), line)?;
+        }
+        Ok(())
+    }
+
+    pub fn is_wall(&self, x: usize, y: usize) -> bool {
+        self.layout[y].chars().nth(x).unwrap() == '─'
+            || self.layout[y].chars().nth(x).unwrap() == '┌'
+            || self.layout[y].chars().nth(x).unwrap() == '│'
+            || self.layout[y].chars().nth(x).unwrap() == '┐'
+            || self.layout[y].chars().nth(x).unwrap() == '└'
+            || self.layout[y].chars().nth(x).unwrap() == '┘'
+    }
+
+    pub fn is_door(&self, x: usize, y: usize) -> bool {
+        self.layout[y].chars().nth(x).unwrap() == '█'
+    }
+
+    pub fn is_bomb(&self, x: usize, y: usize) -> bool {
+        self.layout[y].chars().nth(x).unwrap() == 'B'
+    }
+
+    pub fn spawn_bombs(&mut self, num_bombs: usize) {
+        // Modifier `&self` en `&mut self` pour permettre la modification
+        let mut rng = rand::thread_rng();
+        let mut count = 0;
+
+        while count < num_bombs {
+            let x: usize = rng.gen_range(0..self.width());
+            let y: usize = rng.gen_range(0..self.height());
+
+            if self.layout[y].chars().nth(x) == Some('░') {
+                // Correction: Utiliser une approche qui modifie effectivement la chaîne
+                let mut chars: Vec<char> = self.layout[y].chars().collect();
+                chars[x] = 'B';
+                self.layout[y] = chars.into_iter().collect();
+                count += 1;
+            }
+        }
+    }
+
+    pub fn random_position(&self) -> (u16, u16) {
+        let mut rng = rand::thread_rng();
+        let mut x: u16 = rng.gen_range(0..self.width() as u16);
+        let mut y: u16 = rng.gen_range(0..self.height() as u16);
+        while self
+            .layout
+            .get(y as usize)
+            .and_then(|line| line.chars().nth(x as usize))
+            != Some('▓')
+        {
+            x = rng.gen_range(0..self.width() as u16);
+            y = rng.gen_range(0..self.height() as u16);
+        }
+        (x, y)
+    }
+}
